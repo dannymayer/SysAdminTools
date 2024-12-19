805bb9d274c2a6a9281cc765691fdafbc68210d4
dannymayer <69997043+dannymayer@users.noreply.github.com>
2024-12-19 06:30:51 -0600

    Add Get-RSSFeed.ps1, Update Help on Get-Dated-Directory.ps1

--- source/Public/Get-DatedDirectory.ps1
+++ source/Public/Get-DatedDirectory.ps1
@@ -1,4 +1,15 @@
 function Get-DatedDirectory {
+  <#
+  .SYNOPSIS
+
+  .DESCRIPTION
+
+  .PARAMETER 
+
+  .EXAMPLE
+
+  .NOTES
+  #>
     [string] $CurrentDate = Get-Date -Format "MMddyyyy"
 
     [string] $TargetDatedDirectory = $TargetDirectory + '\' + $CurrentDate
